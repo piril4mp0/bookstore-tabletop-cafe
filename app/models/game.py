cb3c0e516a29d4f06073ca92bea09dfe2ac050c2
piril4mp0 <muriloxd23@gmail.com>
2026-05-15 13:11:17 -0300

    remove unused imports

--- app/models/game.py
+++ app/models/game.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from app.db.database import table_registry
-from sqlalchemy import func
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column, registry
+from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column
+
 
 @mapped_as_dataclass(table_registry)
 class Game:
@@ -16,8 +16,11 @@ class Game:
     - release_date: Release date of the game.
     - players: Number of players supported by the game.
     """
-    __tablename__ = 'games'
-    id: Mapped[int] = mapped_column(init=False, primary_key=True, autoincrement=True, unique=True)
+
+    __tablename__ = "games"
+    id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True, unique=True
+    )
     title: Mapped[str] = mapped_column(unique=True, nullable=False)
     genre: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
