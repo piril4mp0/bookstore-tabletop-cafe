263f2ad2bf57a6fc93a5ab4e919c3e1a6b9a394f
piril4mp0 <muriloxd23@gmail.com>
2026-05-15 12:45:29 -0300

    change dependencies.py to handle generic errors

--- app/dependencies.py
+++ app/dependencies.py
@@ -8,8 +8,11 @@ def get_db() -> Session:
     db = SessionLocal()
     try:
         yield db
-    except Exception:
+    except SQLAlchemyError:
         db.rollback()
         raise SQLAlchemyError("Erro ao realizar operação no banco de dados")
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
