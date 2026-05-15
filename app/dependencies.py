2882a386d6659d37459fe754c31c70dbff3e8c63
piril4mp0 <muriloxd23@gmail.com>
2026-05-14 21:17:54 -0300

    add db rollback if an exception occurs

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,9 +2,12 @@
 from sqlalchemy.orm import Session
 from app.db.database import SessionLocal
 
+
 def get_db() -> Session:
     db = SessionLocal()
     try:
         yield db
+    except Exception:
+        db.rollback()
     finally:
-        db.close()  
+        db.close()
