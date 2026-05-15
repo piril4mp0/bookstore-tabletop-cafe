77ef9076b4cbe35264cdf1dbb074974c38a77b60
piril4mp0 <muriloxd23@gmail.com>
2026-05-15 12:46:28 -0300

    refactor game.py routes to use methods from the service layer

--- app/routers/game.py
+++ app/routers/game.py
@@ -1,47 +1,51 @@
 from http import HTTPStatus
-from typing import Optional
-
 from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.schemas.game import GameCreate, Game, GamePut
-from app.models.game import Game as GameModel
+
+from app.services.GameService import GameService
 
 router = APIRouter(prefix="/games", tags=["games"])
 
+
 ### Creates a new Game ###
-@router.post("/create", response_model=Game, status_code=HTTPStatus.CREATED)
+@router.post("/", response_model=Game, status_code=HTTPStatus.CREATED)
 def create_new_game(new_game: GameCreate = Body(), db: Session = Depends(get_db)):
-    game: GameModel = GameModel(**new_game.model_dump())
-    db.add(game)
-    db.commit()
-    db.refresh(game)
-    return game
+    return GameService.save_game(db, new_game)
+
 
 ### Gets all games or games by genre ###
 @router.get("/", response_model=list[Game], status_code=HTTPStatus.OK)
-def get_games(genre: str | None = Query(default=None, lt=255), db: Session = Depends(get_db)): 
-    # placeholder #
-    games = db.query(GameModel).all()
-    return games
+def get_games(
+    genre: str | None = Query(default=None, max_length=255),
+    db: Session = Depends(get_db),
+):
+    return GameService.get_games(db, genre)
+
 
 ### Gets game by ID ###
 @router.get("/{id}", response_model=Game, status_code=HTTPStatus.OK)
-def get_game(id: int = Path(gt=0), db: Session = Depends(get_db)): 
-    game = db.query(GameModel).filter(GameModel.id == id).first()
+def get_game(id: int = Path(gt=0), db: Session = Depends(get_db)):
+    game = GameService.get_game_by_id(db, id)
+    if not game:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Game not found")
     return game
 
 
 ### PUT game by ID ###
 @router.put("/{id}", response_model=Game, status_code=HTTPStatus.OK)
-def update_game(id: int = Path(gt=0), db: Session = Depends(get_db), updated_game: GamePut = Body()):
-    current_game = db.query(GameModel).filter(GameModel.id == id).first()
-    if not current_game:
+def update_game(
+    id: int = Path(gt=0), updated_game: GamePut = Body(), db: Session = Depends(get_db)
+):
+    game = GameService.update_game(db, id, updated_game)
+    if not game:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Game not found")
-   
-    for key, value in updated_game.model_dump().items():
-        setattr(current_game, key, value)
-    db.commit()
-    db.refresh(current_game)
-    return current_game
+    return game
 
+
+### DELETE game by ID ###
+@router.delete("/{id}", status_code=HTTPStatus.NO_CONTENT)
+def delete_game(id: int = Path(gt=0), db: Session = Depends(get_db)):
+    if not GameService.delete_game(db, id):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Game not found")
